099d94a421f140358c8bc8c9b25f4875ce510f4e
hackerdud3 <nandikantivinay35@gmail.com>
2024-10-02 09:41:26 -0700

    Integrate Pinecone client

---
+++ RAG/pineconeClient.py
@@ -0,0 +1,28 @@
+import getpass
+import os
+import time
+
+from pinecone import Pinecone, ServerlessSpec
+
+if not os.getenv("PINECONE_API_KEY"):
+    os.environ["PINECONE_API_KEY"] = getpass.getpass("Enter your Pinecone API key: ")
+
+pinecone_api_key = os.environ.get("PINECONE_API_KEY")
+
+pc = Pinecone(api_key=pinecone_api_key)
+
+index_name = "pdf-embeddings" 
+
+existing_indexes = [index_info["name"] for index_info in pc.list_indexes()]
+
+if index_name not in existing_indexes:
+    pc.create_index(
+        name=index_name,
+        dimension=3072,
+        metric="cosine",
+        spec=ServerlessSpec(cloud="aws", region="us-east-1"),
+    )
+    while not pc.describe_index(index_name).status["ready"]:
+        time.sleep(1)
+
+index = pc.Index(index_name)

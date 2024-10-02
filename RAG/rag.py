8c1b5e862185976e277a7e5fce7a4e7d0055affa
hackerdud3 <nandikantivinay35@gmail.com>
2024-10-02 09:42:12 -0700

    refactor rag with pinecone vector database

--- RAG/rag.py
+++ RAG/rag.py
@@ -1,38 +1,46 @@
 import re
+import os
 import streamlit as st
 from io import BytesIO
+import os
+import time
+from PyPDF2 import PdfFileReader
+from dotenv import load_dotenv
 from typing import Tuple, List
 from pypdf import PdfReader
 from mongoDBclient import client
 from langchain.docstore.document import Document
-from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_mongodb import MongoDBAtlasVectorSearch
-from load_summarize_chain import load_summarize
+from load_resume_summarize_chain import load_summarize
+from langchain_pinecone import PineconeEmbeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain.embeddings import OpenAIEmbeddings
+from langchain_experimental.text_splitter import SemanticChunker
+import spacy
+import re
 
+load_dotenv()
 
-db_name = "careercoach"
-collection_name = "pdf_embeddings"
+db_name = os.getenv("MONGODB_DATABASE")
+collection_name = os.getenv("MONGODB_EMBEDDING_COLLECTION")
 atlas_collection = client[db_name][collection_name]
-vector_search_index = "pdf_index"
+vector_search_index = os.getenv("MONGODB_VECTOR_SEARCH_INDEX")
+pc_index = os.getenv("PINECONE_INDEX_NAME")
 
-# Parse pdf file
 
-
-def parse_pdf(file: BytesIO) -> Tuple[List[str]]:
-    pdf = PdfReader(file)
-    output = []
-    i = 0
-    for page in pdf.pages:
-        text = page.extract_text()
-        text = re.sub(r"(\w+)-\n(\w+)", r"\1\2", text)
+# Parse PDF
+def parse_pdf(file):
+    pdf_reader = PdfReader(file)
+    text = ""
+    for page_num in range(len(pdf_reader.pages)):
+        page = pdf_reader.pages[page_num]
+        text += page.extract_text()
+        text = re.sub(r"(\w+)-(\w+)", r"\1\2", text)
         text = re.sub(r'\n*•\s*', ' ', text)
         text = re.sub(r"(?<!\n\s)\n(?!\s\n)", "\n", text.strip())
         text = re.sub(r"\n\s*\n", "\n\n", text)
-        output.append(text)
-    return output
-
-# Convert text to chunks
+    return text
 
 
 def text_to_chunks(text, resumeid) -> List[Document]:
@@ -44,9 +52,51 @@ def text_to_chunks(text, resumeid) -> List[Document]:
 
     doc_chunks = []
     for doc in page_docs:
+
+        pattern = r"(\n\n(?:[A-Z\s]+)\n\n)" 
+        chunks = re.split(pattern, doc.page_content) 
+      
+        combined_chunks = []
+        current_chunk = ""
+        for chunk in chunks:
+            if re.match(pattern, chunk):  
+                if current_chunk:
+                    combined_chunks.append(current_chunk)
+                current_chunk = chunk  
+            else:
+                current_chunk += chunk  
+        
+        if current_chunk:  
+            combined_chunks.append(current_chunk)
+
+        for i, chunk in enumerate(combined_chunks):
+            doc = Document(
+                page_content=chunk,
+                metadata={
+                    "page": doc.metadata["page"], "chunk": i, "resumeid": resumeid
+                }
+            )
+            metadata = f"{doc.metadata['page']}-{doc.metadata['chunk']}"
+            doc.metadata["source"] = metadata
+            doc_chunks.append(doc)
+
+    return doc_chunks
+
+
+# Convet text to documents
+def text_to_docs(text, resumeid) -> List[Document]:
+    if isinstance(text, str):
+        text = [text]
+    page_docs = [Document(page_content=page) for page in text]
+    for i, doc in enumerate(page_docs):
+        doc.metadata["page"] = i + 1
+
+    doc_chunks = []
+    for doc in page_docs:
         text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1500,
-            chunk_overlap=20,
+            chunk_size=400,
+            chunk_overlap=0,
+            separators=["?"] 
         )
         chunks = text_splitter.split_text(doc.page_content)
 
@@ -64,26 +114,27 @@ def text_to_chunks(text, resumeid) -> List[Document]:
 
 # Vector store and return index
 
+# def vector_store(docs: List[Document]):
+#     vector_search = PineconeVectorStore.from_documents(
+#         documents=docs,
+#         embedding= PineconeEmbeddings(model="text-embedding-ada-002", api_key=os.getenv("PINECONE_API_KEY")),
+#         index_name=pc_index,
+#         namespace="resumes"
+#     )
+#     return vector_search
 
-def vector_store(docs: List[Document]):
-    vector_search = MongoDBAtlasVectorSearch.from_documents(
-        documents=docs,
-        embedding=OpenAIEmbeddings(disallowed_special=()),
-        collection=atlas_collection,
-        index_name=vector_search_index,
-    )
-    return vector_search
 
+# def get_instance_for_atlas_search(text, resumeid):
+#     documents = []
+#     documents.extend(text_to_chunks(text, resumeid))
+#     extract_text = ""
+#     for doc in documents:
+#         extract_text += doc.page_content
+#     search_instance = vector_store(documents)
+#     return search_instance
 
-def get_index_for_pdf(pdf_files, resumeid, llm):
+def pinecone_vector_store(text, resumeid):
     documents = []
-    for pdf_file in pdf_files:
-        text = parse_pdf(BytesIO(pdf_file))
-
-    doc = Document(page_content=text[0], metadata={"chunk": 0})
-    summarized_pdf = load_summarize([doc], llm)
-    st.session_state["summarized_pdf"] = summarized_pdf
-    st.warning(summarized_pdf)
-    documents.extend(text_to_chunks(summarized_pdf, resumeid))
-    index = vector_store(documents)
-    return index
+    documents.extend(text_to_docs(text, resumeid))
+    st.write(documents)
+    vector_search = PineconeVectorStore.from_documents(documents=documents, embedding= OpenAIEmbeddings(api_key=os.getenv("OPENAI_API_KEY")), index_name=pc_index, namespace=resumeid)

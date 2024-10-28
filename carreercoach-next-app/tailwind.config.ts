3c2a09db7b8d8d8280e1cab8119d68d8a699bdfb
hackerdud3 <nandikantivinay35@gmail.com>
2024-10-28 00:19:06 -0700

    Config files

--- carreercoach-next-app/tailwind.config.ts
+++ carreercoach-next-app/tailwind.config.ts
@@ -11,8 +11,21 @@ module.exports = {
     "./node_modules/@nextui-org/theme/dist/components/(button|snippet|code|input).js",
   ],
   theme: {
-    extend: {},
+  	extend: {
+  		colors: {
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
+  			}
+  		}
+  	}
   },
-  darkMode: "class",
+  darkMode: ["class", "class"],
   plugins: [nextui()],
 };

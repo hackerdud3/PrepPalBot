16445ebaf8b491ee61e9cdb9183706c417a3bf2d
hackerdud3 <nandikantivinay35@gmail.com>
2024-10-28 00:11:37 -0700

    Added resume formatter for efficient retrieval from database

--- RAG/resume_formatter.py
+++ RAG/resume_formatter.py
@@ -3,8 +3,13 @@ from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains.llm import LLMChain
 
-def format_resume(text:str) -> str:
-    llm = ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY"), model="gpt-4o-mini-2024-07-18", temperature=0)
+
+def format_resume(text: str) -> str:
+    llm = ChatOpenAI(
+        api_key=os.getenv("OPENAI_API_KEY"),
+        model="gpt-4o-mini-2024-07-18",
+        temperature=0,
+    )
 
     prompt_template = """
     Your task is to analyze and parse the resume provided below. Identify and extract the following sections:
@@ -18,15 +23,10 @@ def format_resume(text:str) -> str:
     - Activities and Volunteering (if any)
     - Hobbies (if any)
 
-    Here is the resume:
-    Start of resume
-    {text}
-    End of resume
-
-    Please follow these guidelines strictly:
-    - Split the resume into these sections, using a ** question mark (?)** before the section title and **colon (:)** after the section title.
+    Please follow these guidelines and format very strictly:
+    - Split the resume into above sections, using a ** question mark (?)** before section title and **colon (:)** after section title.
     - Within each section, separate individual entries using single line breaks.
-    - Don't make section headings or anything bold, no additional formatting like bullet points or special characters are needed.
+    - Don't make section headings or anything bold, no additional formattings like bullet points, lines, or any special characters are needed.
     - For "Skills", list them as comma-separated values.
     - Avoid excessive spacing and ensure content is concise.
     - Use a clean structure
@@ -42,6 +42,11 @@ def format_resume(text:str) -> str:
     +Projects:
     Project name
     Brief description
+    
+    Here is the resume:
+    <Resume>
+    {text}
+
     """
 
     prompt = PromptTemplate(input_variables=["text"], template=prompt_template)
@@ -49,5 +54,5 @@ def format_resume(text:str) -> str:
     llm_chain = LLMChain(llm=llm, prompt=prompt)
 
     formatted_resume = llm_chain.run({"text": text})
-    
+
     return formatted_resume

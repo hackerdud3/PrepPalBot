8d2177a78f88c0f2cf89aac7610a9b06c7977568
hackerdud3 <nandikantivinay35@gmail.com>
2024-10-28 00:14:13 -0700

    Added seperate pages for resume summary and generate questions

--- RAG/pages/2_Resume_Summary.py
+++ RAG/pages/2_Resume_Summary.py
@@ -2,86 +2,92 @@ import re
 import os
 import streamlit as st
 import nltk
+import time
 from nltk.corpus import stopwords
 from langchain.docstore.document import Document
-from rag import parse_pdf
-from load_resume_summarize_chain import load_summarize
+from rag_chain import parse_pdf
+from resume_summarize_chain import load_summarize
 from langchain_openai import ChatOpenAI
 
 
 # Clean Resume file
 def clean_resume(resume_text: str) -> str:
     # Download the stopwords list
-    nltk.download('stopwords')
-    stop_words = set(stopwords.words('english'))
+    nltk.download("stopwords")
+    stop_words = set(stopwords.words("english"))
 
-    cleaned_resume = re.sub(r"\n+", ". ", resume_text)
-    
+    cleaned_resume = re.sub(r"\n+", " ", resume_text)
+
     # Tokenize the text into words
-    words = re.findall(r'\b\w+\b|\.|\,', cleaned_resume)
+    words = re.findall(r"\b\w+\b|\.|\,|\-", cleaned_resume)
 
     # Remove stopwords
     cleaned_words = [word for word in words if word not in stop_words]
-    cleaned_description = ' '.join(cleaned_words)
+    cleaned_description = " ".join(cleaned_words)
 
     return cleaned_description
+
+
+def stream_data(text: str):
+    for word in text.split(" "):
+        yield word + " "
+        time.sleep(0.02)
 
 
 def main():
     st.title("📝 Summarize Resume")
 
-    llm = ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY"), model="gpt-4o-mini-2024-07-18", temperature=0)
-    
+    llm = ChatOpenAI(
+        api_key=os.getenv("OPENAI_API_KEY"),
+        model="gpt-4o-mini-2024-07-18",
+        temperature=0.5,
+    )
+
     # File uploader for the resume (PDF only)
-    resume_pdf = st.file_uploader(label="Upload Resume", type="pdf", accept_multiple_files=False)
+    resume_pdf = st.file_uploader(
+        label="Upload Resume", type="pdf", accept_multiple_files=False
+    )
 
     # Store the uploaded resume in session state
     if resume_pdf:
         st.session_state["resume_pdf"] = resume_pdf
-    
+        st.session_state["summarized_resume"] = ""
+
     # Button to trigger resume summarization
     summarize = st.button(label="Summarize Resume")
 
-        # Initialize session state for username if not present
+    # Initialize session state for username if not present
     if "username" not in st.session_state:
         st.session_state["username"] = ""
-
-    if "summarized_resume" in st.session_state:
-        st.markdown("### Resume Summary:")
-        st.warning(st.session_state["summarized_resume"])
 
     if summarize:
         st.session_state["summarized_resume"] = None
 
         has_error = False
-        
+
         # Check if resume is uploaded
         if resume_pdf is None:
             st.error("Please upload resume")
             has_error = True
-        
+
         # # Username validation
         # if st.session_state["username"] == "":
         #     st.error("Please enter username")
         #     has_error = True
-        
+
         if has_error:
             return
-        
-        resume_text = parse_pdf(resume_pdf)
 
-        cleaned_resume = clean_resume(resume_text)
+        with st.spinner("Thinking..."):
+            resume_text = parse_pdf(resume_pdf)
+            cleaned_resume = clean_resume(resume_text)
 
-        st.write(cleaned_resume)
+        with st.spinner("Summarizing..."):
+            summarized_pdf = load_summarize(cleaned_resume, llm)
+            st.markdown("### Resume Summary")
+            st.write_stream(stream_data(summarized_pdf))
+            st.session_state["summarized_resume"] = summarized_pdf
 
-        doc = Document(page_content=cleaned_resume, metadata={"chunk": 0})
-
-        summarized_pdf = load_summarize([doc], llm)
-        
-        st.session_state["summarized_resume"] = summarized_pdf
-        
-        st.warning(st.session_state["summarized_resume"])
-    
 
 if __name__ == "__main__":
     main()
--- RAG/pages/3_Generate_Questions.py
+++ RAG/pages/3_Generate_Questions.py
@@ -6,14 +6,19 @@ from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
 
+
 def generate_questions_from_stuffed_docs(url_chunks: List[str]):
-    llm = ChatOpenAI(api_key=os.getenv("OPENAI_API_KEY"), model="gpt-4o-mini-2024-07-18", temperature=0.2)
+    llm = ChatOpenAI(
+        api_key=os.getenv("OPENAI_API_KEY"),
+        model="gpt-4o-mini-2024-07-18",
+        temperature=0.2,
+    )
 
     prompt_template = """
     You are a helpful assistant tasked with training users on personalized interview questions. Your goal is to identify 10 interview questions and provide corresponding answers based on the context provided below, in a sequential manner.
     Provide questions only if the context is relevant to interviews.
 
-    Your response should follow this format:
+    Your response should strictly follow this format:
     For example:
     1. Interviewer: Tell me about yourself and your qualifications?
     \nAnswer: Certainly! Over the past three years, I've been working as a bookkeeper, where I've managed accounts payable and receivable, along with payroll oversight. I take pride in my ability to identify and resolve discrepancies, leading to substantial cost savings for my company. Recently, I earned my CPA degree, and I'm confident that my expertise in bookkeeping, coupled with my attention to detail, aligns well with the requirements of the public accountant role at your organization.
@@ -27,17 +32,15 @@ def generate_questions_from_stuffed_docs(url_chunks: List[str]):
 
     stuff_chain = create_stuff_documents_chain(llm=llm, prompt=stuff_prompt)
 
-    output = stuff_chain.invoke({"context": url_chunks})
-
-    return output
+    return stuff_chain.stream({"context": url_chunks})
 
 
 def main():
     st.title("🔗 Generate Questions")
-   
-    url = st.text_input("Careersite URL", placeholder= "https://www.example.com")
-    if 'user_name' not in st.session_state:
-        st.session_state["user_name"] = ""
+
+    url = st.text_input("Careersite URL", placeholder="https://www.example.com")
+    if "user_name" not in st.session_state:
+        st.session_state["user_name"] = "Vinay"
 
     process_url = st.button("Generate Interview Questions")
 
@@ -52,16 +55,18 @@ def main():
         if st.session_state["user_name"] == "":
             st.error("Please enter username to continue")
             return
-        
+
         url_chunks = get_url_chunks(url, st.session_state["user_name"])
         url_chunks = url_chunks[:10]
 
         with st.spinner("Generating Interview Questions..."):
-            questions_and_answers = generate_questions_from_stuffed_docs(url_chunks)
-            st.write(questions_and_answers)
+            st.markdown("### Interview Questions and Answers from the website")
+            questions_and_answers = st.write_stream(
+                generate_questions_from_stuffed_docs(url_chunks)
+            )
             st.session_state["generated_questions"] = questions_and_answers
-        
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
 
     main()
